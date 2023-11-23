b4e8ff1f1ec7dd7a175438d28c72b9cfb4d339cc
Shim Yunseop <97441805+shimyounseob@users.noreply.github.com>
2023-11-23 14:46:34 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 220 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    
+	public static void main(String[] args) {
+       
+	Scanner sc = new Scanner(System.in) ;
+	int N = sc.nextInt() ; 
+	
+	System.out.println(factorial(N)); 
+	
+	}
+	
+	public static long factorial (int n) {
+		
+		if (n == 0) {
+			return 1 ; 
+		}
+		if (n == 1) {
+			return 1 ; 
+		}
+		return n*factorial(n-1) ; 
+	}
+}
